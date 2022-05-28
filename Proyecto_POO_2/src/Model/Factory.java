58a5c20b131f2cdf7f58902a686fa2fdb2157742
Yess Solano <90736001+yessmsr@users.noreply.github.com>
2022-05-28 17:04:33 -0600

    Update Factory.java

--- Proyecto_POO_2/src/Model/Factory.java
+++ Proyecto_POO_2/src/Model/Factory.java
@@ -2,5 +2,13 @@ package Model;
 
 public class Factory {
     
+    public Factory(){}
+
+    public Aliado crearAliado(){
+        return new Aliado();
+    }
+    public Enemigo crearEnemigo(){
+        return new Enemigo();
+    }
     
 }
